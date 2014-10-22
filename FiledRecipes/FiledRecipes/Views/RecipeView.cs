c8358673b3f7652252e2eb2227b6168b204f5dd1
hj222hi <hj222hi@student.lnu.se>
2014-10-22 17:15:48 +0200

    lite kod

--- FiledRecipes/FiledRecipes/Views/RecipeView.cs
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -4,6 +4,7 @@ using FiledRecipes.Properties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace FiledRecipes.Views
 {
@@ -12,6 +13,25 @@ namespace FiledRecipes.Views
     /// </summary>
     public class RecipeView : ViewBase, IRecipeView
     {
-        hohjhojhohj ho
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader("Recipes.txt"))
+                {
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Fel inträffade vid läsning av textfil.");
+            }
+    }
     }
 }
